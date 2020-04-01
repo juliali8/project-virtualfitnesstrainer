ef8f56fb03ca732152ef52b7494a5ed0916bd0ff
Juila Li <lijulia@wustl.edu>
2020-04-01 01:17:48 -0500

    changed a textbox to label

--- src/virtualfitnesstrainer/BeginWindow.java
+++ src/virtualfitnesstrainer/BeginWindow.java
@@ -3,6 +3,16 @@ package virtualfitnesstrainer;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.SpringLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
 
 public class BeginWindow {
 
@@ -36,8 +46,33 @@ public class BeginWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		SpringLayout springLayout = new SpringLayout();
+		frame.getContentPane().setLayout(springLayout);
+		
+		JButton btnBeginNow = new JButton("BEGIN NOW");
+		springLayout.putConstraint(SpringLayout.NORTH, btnBeginNow, 178, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, btnBeginNow, -71, SpringLayout.SOUTH, frame.getContentPane());
+		btnBeginNow.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnBeginNow.setBackground(Color.WHITE);
+		btnBeginNow.setForeground(new Color(0, 128, 0));
+		springLayout.putConstraint(SpringLayout.WEST, btnBeginNow, -286, SpringLayout.EAST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, btnBeginNow, -170, SpringLayout.EAST, frame.getContentPane());
+		btnBeginNow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		frame.getContentPane().add(btnBeginNow);
+		
+		JLabel lblWelcomeToCovid = new JLabel("Welcome to COVID-19 FITNESS TRAINER!");
+		springLayout.putConstraint(SpringLayout.NORTH, lblWelcomeToCovid, 79, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lblWelcomeToCovid, -93, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(lblWelcomeToCovid);
 	}
-
 }
