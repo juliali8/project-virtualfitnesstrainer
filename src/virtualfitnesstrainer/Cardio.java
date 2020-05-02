812d8b913d369abb987979ed3e7ba1acc033c27f
Kyle Joseph <k.joseph@wustl.edu>
2020-05-02 01:43:38 -0400

    added filepath variable so images can be found for all exercises

--- src/virtualfitnesstrainer/Cardio.java
+++ src/virtualfitnesstrainer/Cardio.java
@@ -1,14 +1,17 @@
 package virtualfitnesstrainer;
 
 public class Cardio implements Exercise{
+	
 	String name; 
 	int duration;
 	String[] equipment; 
+	String imagePath;
 	
 	public Cardio(String name, int duration, String[] equipment) {
 		this.name = name; 
 		this.duration = duration; 
-		this.equipment = equipment; 
+		this.equipment = equipment;
+		this.imagePath = "./images/cardio/" + name + ".jpg";
 	}
 	
 	public String getName() {
@@ -26,6 +29,10 @@ public class Cardio implements Exercise{
 
 	public String[] getEquipment() {
 		return equipment;
+	}
+	
+	public String getImage() {
+		return imagePath;
 	}
 	
 	public String toString() {
